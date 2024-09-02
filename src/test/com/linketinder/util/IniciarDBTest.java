ad6288f0b3156946b7f0fad217eaf9a6ff556c49
André Hopf <andre.hopf@gmail.com>
2024-09-02 15:54:54 -0300

    feat: add testes unitarios para IniciarDB

---
+++ src/test/com/linketinder/util/IniciarDBTest.java
@@ -0,0 +1,39 @@
+package com.linketinder.util;
+
+import com.linketinder.model.Candidato;
+import com.linketinder.model.Empresa;
+import com.linketinder.service.CandidatoService;
+import com.linketinder.service.EmpresaService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class IniciarDBTest {
+    CandidatoService candidatoServiceMock = mock(CandidatoService.class);
+    EmpresaService empresaServiceMock = mock(EmpresaService.class);
+
+    @Nested
+    @DisplayName("Método: iniciar")
+    class IniciarTests {
+        @Test
+        @DisplayName("Quando método iniciar é invocado, deve chamar os métodos adicionarCandidato e adicionarEmpresa 5 vezes")
+        void iniciarTest() {
+            //arrange
+            when(candidatoServiceMock.adicionarCandidato(any(Candidato.class))).thenReturn(new Candidato());
+            when(empresaServiceMock.adicionarEmpresa(any(Empresa.class))).thenReturn(new Empresa());
+
+            //act
+            IniciarDB.iniciar(candidatoServiceMock, empresaServiceMock);
+
+            //assert
+            verify(candidatoServiceMock, times(5)).adicionarCandidato(any(Candidato.class));
+            verify(empresaServiceMock, times(5)).adicionarEmpresa(any(Empresa.class));
+        }
+    }
+
+
+
+}
